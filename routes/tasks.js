2c82309247a2ea4813851fdc915ecb5532873742
mason1225 <75379891+mason1225@users.noreply.github.com>
2021-10-13 00:13:59 -0400

    Created post route

--- routes/tasks.js
+++ routes/tasks.js
@@ -9,6 +9,19 @@ router.get('/', (req, res) => {
 });
 // GET - One
 // CREATE
+router.post('/', async (req, res) => {
+    const task = new Task({
+        title: req.body.title,
+        dateOfEvent: req.body.dateOfEvent,
+        description: req.body.description
+    });
+    try {
+        const newTask = await task.save();
+        res.status(201).json({ message: "Task saved successfully" });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+})
 // UPDATE
 // DELETE
 
